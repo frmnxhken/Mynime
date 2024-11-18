c7b5ac2575f354bcc5046eba381313ad2885a819
Wahyudi Firmansyah <frmnxhken@gmail.com>
2024-11-18 17:14:15 +0700

    Update Board.jsx

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -6,7 +6,7 @@ const Board = (props) => {
     const today = days[new Date().getDay()]
 
     return (
-        <div className={`${day === today ? 'bg-yellow-50' : 'bg-white'} border border-gray-300 px-4 py-6`}>
+        <div className={`${day === today ? 'bg-green-500 text-white' : 'bg-white border border-gray-300'} px-4 py-6`}>
             <div>
                 <p className='text-center text-md font-semibold'>{day[0].toUpperCase() + day.slice(1)}</p>
             </div>
@@ -14,7 +14,7 @@ const Board = (props) => {
                 <ul className='space-y-2'>
                     {schedules && schedules[day] ? (
                         schedules[day].map((schedule, index) => (
-                            <li key={index} className='text-sm border-b border-gray-300 px-4 py-2'>
+                            <li key={index} className={`text-sm border-b ${day === today ? 'border-black' : 'border-gray-300'} px-4 py-2`}>
                                 {schedule}
                             </li>
                         ))
