7d3000ccc44e236a5dfd8351344a0ee4fc3897ef
Wahyudi Firmansyah <frmnxhken@gmail.com>
2024-12-14 11:20:46 +0700

    Update Endpoint.js

--- src/utils/Endpoint.js
+++ src/utils/Endpoint.js
@@ -1,4 +1,4 @@
-const BASE_API = 'https://anime.sipenyumuda.biz.id/api'
+const BASE_API = 'http://35.208.71.15:8000/api'
 export const LIST_ANIME = BASE_API + '/anime'
 export const STREAM = BASE_API + '/episode/'
 export const SCHEDULE = BASE_API + '/schedule'
