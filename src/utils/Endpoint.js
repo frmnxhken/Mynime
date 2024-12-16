2eb64c77d31b89c1cef8e25b4a9978d976192259
Wahyudi Firmansyah <frmnxhken@gmail.com>
2024-12-16 16:55:30 +0700

    Update Endpoint.js

--- src/utils/Endpoint.js
+++ src/utils/Endpoint.js
@@ -1,4 +1,4 @@
-const BASE_API = 'http://35.208.71.15:8000/api'
+const BASE_API = 'https://sesatnime.serv00.net/api'
 export const LIST_ANIME = BASE_API + '/anime'
 export const STREAM = BASE_API + '/episode/'
 export const SCHEDULE = BASE_API + '/schedule'
